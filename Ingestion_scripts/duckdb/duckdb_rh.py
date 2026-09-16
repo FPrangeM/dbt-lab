e9e4302dfc1959b63d88ef54a21b38dbbc662c61
Fernando Prange <fpmenechini@gmail.com>
2026-09-16 08:41:22 -0300

    Added unit test. Other tests were made on explorer..ipynb

--- Ingestion_scripts/duckdb/duckdb_rh.py
+++ Ingestion_scripts/duckdb/duckdb_rh.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine,text
 
 
 df = duckdb.sql("""
-select * from read_csv('./data/extract_rh*.csv',all_varchar=True,filename=True)
+select * from read_csv('./data/extract_rh_2026-07-27_erros.csv',all_varchar=True,filename=True)
 """).df()
 # select * from read_csv('./data/extract_rh_2026-07-13.csv',all_varchar=True,filename=True)
 
---
+++ dbt_lab/tests/rh_desligados.sql
@@ -0,0 +1,15 @@
+{{ config(
+    severity='warn',
+    tags=['qualidade', 'rh', 'observabilidade'],
+    store_failures=true
+) }}
+
+SELECT
+    ref_data,
+    cpf,
+    nome,
+    status,
+    data_desligamento
+FROM {{ ref('rh_trusted') }}
+WHERE status = 'DESLIGADO'
+  AND data_desligamento IS NULL
